d99f31b25643e331bfc21a2b04a84ed40b6bfde2
Uviwe Dana <uviwedana09@gmail.com>
2023-03-28 16:35:59 +0200

    new updates

---
+++ emoji switch/index.js
@@ -0,0 +1,27 @@
+const closedEmoji = document.querySelector('.closed')
+const openEmoji = document.querySelector('.open')
+
+// Even Listener
+
+closedEmoji.addEventListener('click', () => {
+
+    if(openEmoji.classList.contains('open')) {
+
+        openEmoji.classList.add('active')
+        closedEmoji.classList.remove('active');
+    }
+
+ })
+
+ openEmoji.addEventListener('click', () => {
+
+    if(closedEmoji.classList.contains('closed')) {
+    
+    closedEmoji.classList.add('active');
+    openEmoji.classList.remove('active');
+
+    }
+
+ })
+   
+
