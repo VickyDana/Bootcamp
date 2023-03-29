fe3116cf7b9cd80d4de513c2f77061d99a5fb99c
Uviwe Dana <uviwedana09@gmail.com>
2023-03-29 16:12:21 +0200

    fith commit

---
+++ fundamental/fundamental.js
@@ -0,0 +1,69 @@
+/*var a= 1;
+var a= 10;
+*/
+/*let a= 1;
+let b= 2;
+let c= a%b;
+*/
+//let a= 10;
+//let b= 20;
+//let c= 70
+//let d= 10
+
+//if (a=b){
+    //console.log("True");
+//}else{
+   // console.log("False"); 
+
+//}
+
+/*let a= 20;
+let b= 5;
+let c
+
+let c= let a
+let b= let
+*/
+
+/*let a = -10
+
+if (a > 10) {
+
+    console.log("Greater than a decade");
+}else if (a==10) {
+    console.log("it is a decade")
+}else if (a < 10 || a >= 0){
+    console.log("it is lesser than a decade")
+}else{
+   console.log("we cannot have negative years!")
+}
+*/
+
+let a= 4;
+let b= 12;
+let c= 8;
+
+if (a > b && a > c) {
+    console.log("a cannot be maximum number")
+}else if (b > a && b > c){
+    console.log("b is a maximum number")
+}else if (c > a && c > b) {
+    console.log("c is a middle number")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
