3ef0636a137c125b0ffda9c3bb35b90b66b62d26
Uviwe Dana <uviwedana09@gmail.com>
2023-04-03 16:51:11 +0200

    second arrays

--- fundamental/array.js
+++ fundamental/array.js
@@ -16,7 +16,7 @@ console.log(array)*/
 
 
 
-array=[20,311,12,11,13,15,9,4,0,10]
+/*array=[20,311,12,11,13,15,9,4,0,10]
 let Even = new Array();
 let Odd =new Array();
 
@@ -26,7 +26,7 @@ for(let i=0; i < array.length;i++){
     }else {
         Odd.push(array[i]);
     }
-    
+
     let Object={
         Even,
         Odd,
@@ -35,7 +35,94 @@ for(let i=0; i < array.length;i++){
 }
 
     console.log(Even);
-    console.log(Odd);
+    console.log(Odd);*/
+
+    /*let numbers= [-3,-100,10,4,6,7,20,1]
+
+    let letters = ["d","a","e","g","c","f","b","h"];
+   numbers.sort((a,b)=> a-b)
+    console.log(numbers)*/
+
+
+   /*let letters = ["d","a","e","g","c","f","b","h"];
+   letters.sort()
+    console.log(letters)*/
+
+    /*let Name= "uviwe"
+    let reversedName= "ewivu"
+    console.log(Name)*/
+
+     /*function reversedmyName(myName){
+     let newName= "";
+
+     for(let i= myName; i >= 0;i--){
+         newName += myName [i];
+     }
+    return newName
+    }
+     console.log(reversedmyName("uviwe"))*/
+
+
+/*function strReversed (text){
+    let reversed ="";
+    for(let i=0; i < text.length; i++){
+
+    reversed=text[i] + reversed;
+}
+console.log(reversed);
+
+}
+
+strReversed("I am testing")
+strReversed()*/
+
+//Parameters
+
+/*function addThreeParameters (hourlyrate, hours, days){
+    let salary = hourlyrate*hours* days
+    console.log(salary)
+}
+addThreeParameters (26,8,25)*/
+
+    
+/*function addThreeParameters (hourlyrate,hours,days,pci=1){
+    return hourlyrate*hours*days*pci
+}
+console.log(addThreeParameters (26,8))*/
+
+/*function emailGen(fname="Vicky",Lname="Dana") {
+    //return fname+"."+ Lname+ "@gsb.co.za"
+    let email=fname + "." + Lname + "@gsb.co.za"
+    return email.toUpperCase()
+}
+console.log(emailGen())
+console.log(emailGen("uviwe dana"))*/
+
+ //Spread-Operator
+
+let num1=[1,3,5,7]
+let num2=[0,2,4,6]
+let num3=[num1 ,num2]
+let num4= num1.concat(num2)
+
+console.log("This is num3", num3)
+console.log("This is num4", num4)
+
+    
+
+
+
+
+
+
+
+
+   
+    
+    
+        
+    
+    
 
 
     
