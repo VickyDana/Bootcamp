ac252c30f26a938a3e56f3d77c9d9d71c4583269
Uviwe Dana <uviwedana09@gmail.com>
2023-04-04 16:51:27 +0200

    functions

---
+++ born year/age.js
@@ -0,0 +1,30 @@
+ /*function calcAge (startYear,endYear){
+
+   let Age= startYear-endYear
+ 
+
+if(Age < 0){
+    console.log("Age can not be negative number")
+}else if(Age = 21) {
+    return "Please enter your born year"
+
+}
+    console.log(Age)
+}
+
+calcAge(2023,2002)*/
+
+/*function calcAge (currentYear,bornYear) {
+    let age= currentYear-bornYear
+    
+    if(age != -21){
+        return "Please enter your born year"
+    }
+    else {
+        return "right"
+    }
+    }
+    
+
+
+console.log(calcAge(2002,2023))*/
---
+++ born year/arithmetic.js
@@ -0,0 +1,24 @@
+
+function addTen (r){
+    let num = r + 10;
+    return num;
+}
+console.log(addTen(4));
+
+function multiplyFive (s){
+    let num = s * 5;
+    return num;
+}
+console.log(multiplyFive(14))
+
+function addTwoValues (r,s){
+    let sum= r+s
+    return r+s
+}
+console.log(14+70)
+
+
+
+
+
+
